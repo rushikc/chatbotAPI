e6f7ee215969fc857a1d5aa9e55d0598ddade9cd
rushikc <1ms16cs021@msrit.edu>
2019-09-22 22:13:04 +0530

    update

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const jsonParser = bodyParser.json();
 const functions = require('./config/comman');
 
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 
 app.get('/',(req,res)=>{
     res.send("working");
